fd68e30e7dc3d38be59358c1e425107ea03a106b
suleymaneser <esersuleyman96@gmail.com>
2021-01-15 00:47:49 +0300

    added ui

---
+++ ui/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Product/>
+</template>
+
+<script>
+import Product from './components/Product.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Product
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #f6f6f6;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
